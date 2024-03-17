b0118e90d58ccb8c537368321849029981b36810
fuadmuhe12 <fuad.mohamed@aait.edu.et>
2024-03-17 07:00:28 +0300

    Added New feature (selecting city)

--- lib/weather_cards.dart
+++ lib/weather_cards.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:weather_app/cards.dart';
 
 class WeatherCard extends StatefulWidget {
--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -5,25 +5,26 @@ import 'package:weather_app/addtional_info.dart';
 import 'package:weather_app/main_card.dart';
 import 'package:weather_app/weather_cards.dart';
 import 'package:http/http.dart' as http;
+import 'package:dropdown_search/dropdown_search.dart';
 
-Future getData() async {
-  Uri url = Uri(
-      scheme: 'http',
-      host: 'api.weatherapi.com',
-      path: 'v1/current.json',
-      queryParameters: {
-        'key': '9c814d122736458db8452447240903',
-        'q': 'Addis Ababa',
-        'aqi': 'yes'
-      });
-  try {
-    var responce = await http.get(url);
-    var data = jsonDecode(responce.body);
-    return data;
-  } catch (e) {
-    return {};
-  }
-}
+// Future getData(String country) async {
+//   Uri url = Uri(
+//       scheme: 'http',
+//       host: 'api.weatherapi.com',
+//       path: 'v1/current.json',
+//       queryParameters: {
+//         'key': '9c814d122736458db8452447240903',
+//         'q': country,
+//         'aqi': 'yes'
+//       });
+//   try {
+//     var responce = await http.get(url);
+//     var data = jsonDecode(responce.body);
+//     return data;
+//   } catch (e) {
+//     return {};
+//   }
+// }
 
 class MyWeatherPage extends StatefulWidget {
   const MyWeatherPage({super.key});
@@ -33,16 +34,18 @@ class MyWeatherPage extends StatefulWidget {
 }
 
 class _MyWeatherPagestate extends State {
+
+  String? selectedItem = 'Addis Ababa' ;
   Map<String, dynamic> data = {};
   //get the data from the API
-  Future getData() async {
+  Future getData({String? country}) async {
     Uri url = Uri(
         scheme: 'http',
         host: 'api.weatherapi.com',
         path: 'v1/current.json',
         queryParameters: {
           'key': '9c814d122736458db8452447240903',
-          'q': 'Addis Ababa',
+          'q': country ?? 'Addis Ababa',
           'aqi': 'yes'
         });
     try {
@@ -57,16 +60,15 @@ class _MyWeatherPagestate extends State {
   @override
   void initState() {
     super.initState();
-    getData();
+    getData(country: selectedItem);
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      
-      future: getData(),
+      future: getData(country: selectedItem),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting  ) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
         }
         return Scaffold(
@@ -77,7 +79,7 @@ class _MyWeatherPagestate extends State {
               IconButton(
                   onPressed: () {
                     setState(() {
-                      
+                      getData();
                     });
                   },
                   icon: const Icon(Icons.refresh))
@@ -87,10 +89,289 @@ class _MyWeatherPagestate extends State {
               ? const Center(child: CircularProgressIndicator())
               : ListView(
                   children: [
+                    DropdownSearch<String>(
+                      selectedItem: selectedItem,
+                    onChanged:(value) {
+                      setState(() {
+                        selectedItem = value;
+                      });
+                    } ,
+                    
+                      popupProps: PopupProps.menu(
+                        showSearchBox: true,
+                        showSelectedItems: true,
+                        disabledItemFn: (String s) => s.startsWith('I'),
+                      ),
+                      items: const [
+                        'Addis Ababa',
+                        "Tokyo",
+                        "New Delhi",
+                        "Washington, D.C.",
+                        "Beijing",
+                        "Moscow",
+                        "London",
+                        "Paris",
+                        "Berlin",
+                        "Rome",
+                        "Canberra",
+                        "Brasília",
+                        "Ottawa",
+                        "Ankara",
+                        "Madrid",
+                        "Stockholm",
+                        "Cairo",
+                        "Dublin",
+                        "Mexico City",
+                        "Buenos Aires",
+                        "Brussels",
+                        "Bogotá",
+                        "Santiago",
+                        "Bangkok",
+                        "Athens",
+                        "Helsinki",
+                        "Oslo",
+                        "Amsterdam",
+                        "Vienna",
+                        "Islamabad",
+                        "Dhaka",
+                        "Nairobi",
+                        "Kuala Lumpur",
+                        "Manila",
+                        "Lisbon",
+                        "Warsaw",
+                        "Seoul",
+                        "Singapore",
+                        "Wellington",
+                        "Jakarta",
+                        "Hanoi",
+                        "Budapest",
+                        "Prague",
+                        "Copenhagen",
+                        "Tehran",
+                        "Jerusalem",
+                        "Kiev",
+                        "Manama",
+                        "Doha",
+                        "Abu Dhabi",
+                        "Muscat",
+                        "Kuwait City",
+                        "San José",
+                        "Panama City",
+                        "Havana",
+                        "Caracas",
+                        "Lima",
+                        "San Salvador",
+                        "Guatemala City",
+                        "Tegucigalpa",
+                        "San Salvador",
+                        "Guatemala City",
+                        "Tegucigalpa",
+                        "San Juan",
+                        "Santo Domingo",
+                        "Port-au-Prince",
+                        "Kingston",
+                        "Nassau",
+                        "Belmopan",
+                        "Paramaribo",
+                        "Georgetown",
+                        "Bridgetown",
+                        "Port of Spain",
+                        "Castries",
+                        "Apia",
+                        "Port Vila",
+                        "Honiara",
+                        "Palikir",
+                        "Majuro",
+                        "Funafuti",
+                        "Tarawa",
+                        "Yaren",
+                        "South Tarawa",
+                        "Avarua",
+                        "Pago Pago",
+                        "Ngerulmud",
+                        "Hamilton",
+                        "Charlotte Amalie",
+                        "Gustavia",
+                        "Saint-Pierre",
+                        "The Valley",
+                        "Oranjestad",
+                        "Philipsburg",
+                        "Cockburn Town",
+                        "Basse-Terre",
+                        "Fort-de-France",
+                        "George Town",
+                        "Road Town",
+                        "Jamestown",
+                        "Adamstown",
+                        "Flying Fish Cove",
+                        "Kingston",
+                        "Alofi",
+                        "Hagåtña",
+                        "Mata-Utu",
+                        "Avarua",
+                        "Willemstad",
+                        "The Bottom",
+                        "Kralendijk",
+                        "Grytviken",
+                        "Longyearbyen",
+                        "Marigot",
+                        "Gibraltar",
+                        "Saint Helier",
+                        "Saint Peter Port",
+                        "Vaduz",
+                        "Saint Peter Port",
+                        "Douglas",
+                        "St. Helier",
+                        "Road Town",
+                        "Jamestown",
+                        "Adamstown",
+                        "Flying Fish Cove",
+                        "Kingston",
+                        "Alofi",
+                        "Hagåtña",
+                        "Mata-Utu",
+                        "Avarua",
+                        "Willemstad",
+                        "The Bottom",
+                        "Kralendijk",
+                        "Grytviken",
+                        "Longyearbyen",
+                        "Marigot",
+                        "Gibraltar",
+                        "Saint Helier",
+                        "Saint Peter Port",
+                        "Vaduz",
+                        "Saint Peter Port",
+                        "Douglas",
+                        "St. Helier",
+                        "Road Town",
+                        "Jamestown",
+                        "Adamstown",
+                        "Flying Fish Cove",
+                        "Kingston",
+                        "Alofi",
+                        "Hagåtña",
+                        "Mata-Utu",
+                        "Avarua",
+                        "Willemstad",
+                        "The Bottom",
+                        "Kralendijk",
+                        "Grytviken",
+                        "Longyearbyen",
+                        "Marigot",
+                        "Gibraltar",
+                        "Saint Helier",
+                        "Saint Peter Port",
+                        "Vaduz",
+                        "Saint Peter Port",
+                        "Douglas",
+                        "St. Helier",
+                        "Road Town",
+                        "Jamestown",
+                        "Adamstown",
+                        "Flying Fish Cove",
+                        "Kingston",
+                        "Alofi",
+                        "Hagåtña",
+                        "Mata-Utu",
+                        "Avarua",
+                        "Willemstad",
+                        "The Bottom",
+                        "Kralendijk",
+                        "Grytviken",
+                        "Longyearbyen",
+                        "Marigot",
+                        "Gibraltar",
+                        "Saint Helier",
+                        "Saint Peter Port",
+                        "Vaduz",
+                        "Saint Peter Port",
+                        "Douglas",
+                        "St. Helier",
+                        "Road Town",
+                        "Jamestown",
+                        "Adamstown",
+                        "Flying Fish Cove",
+                        "Kingston",
+                        "Alofi",
+                        "Hagåtña",
+                        "Mata-Utu",
+                        "Avarua",
+                        "Willemstad",
+                        "The Bottom",
+                        "Kralendijk",
+                        "Grytviken",
+                        "Longyearbyen",
+                        "Marigot",
+                        "Gibraltar",
+                        "Saint Helier",
+                        "Saint Peter Port",
+                        "Vaduz",
+                        "Saint Peter Port",
+                        "Douglas",
+                        "St. Helier",
+                        "Road Town",
+                        "Jamestown",
+                        "Adamstown",
+                        "Flying Fish Cove",
+                        "Kingston",
+                        "Alofi",
+                        "Hagåtña",
+                        "Mata-Utu",
+                        "Avarua",
+                        "Willemstad",
+                        "The Bottom",
+                        "Kralendijk",
+                        "Grytviken",
+                        "Longyearbyen",
+                        "Marigot",
+                        "Gibraltar",
+                        "Saint Helier",
+                        "Saint Peter Port",
+                        "Vaduz",
+                        "Saint Peter Port",
+                        "Douglas",
+                        "St. Helier",
+                        "Road Town",
+                        "Jamestown",
+                        "Adamstown",
+                        "Flying Fish Cove",
+                        "Kingston",
+                        "Alofi",
+                        "Hagåtña",
+                        "Mata-Utu",
+                        "Avarua",
+                        "Willemstad",
+                        "The Bottom",
+                        "Kralendijk",
+                        "Grytviken",
+                        "Longyearbyen",
+                        "Marigot",
+                        "Gibraltar",
+                        "Saint Helier",
+                        "Saint Peter Port",
+                        "Vaduz",
+                        "Saint Peter Port",
+                        "Douglas",
+                        "St. Helier",
+                        "Road Town",
+                        "Jamestown",
+                        "Adamstown",
+                        "Flying Fish"
+                      ],
+                      dropdownDecoratorProps: const DropDownDecoratorProps(
+                        dropdownSearchDecoration: InputDecoration(
+                          labelText: "Selected City",
+                          hintText: "Select the city",
+                        ),
+                      ),
+
+                    ),
                     MainCard(
                         curtemp: snapshot.data['current']['temp_c'].toString(),
                         icon: Icons.sunny,
-                        cond: snapshot.data['current']['condition']['text'].toString()),
+                        cond: snapshot.data['current']['condition']['text']
+                            .toString()),
                     const SizedBox(
                       height: 20,
                     ),
@@ -120,19 +401,22 @@ class _MyWeatherPagestate extends State {
                         AdditionalInfo(
                             label: 'Humidity',
                             value: snapshot.data.length > 1
-                                ? snapshot.data['current']['humidity'].toString()
+                                ? snapshot.data['current']['humidity']
+                                    .toString()
                                 : "Looding ",
                             icon: Icons.water_drop),
                         AdditionalInfo(
                             label: 'Wind',
                             value: snapshot.data.length > 1
-                                ? snapshot.data['current']['wind_kph'].toString()
+                                ? snapshot.data['current']['wind_kph']
+                                    .toString()
                                 : 'loading',
                             icon: Icons.air),
                         AdditionalInfo(
                             label: 'pressure',
                             value: snapshot.data.length > 1
-                                ? snapshot.data['current']['pressure_mb'].toString()
+                                ? snapshot.data['current']['pressure_mb']
+                                    .toString()
                                 : 'loading',
                             icon: Icons.south_america_sharp)
                       ],
